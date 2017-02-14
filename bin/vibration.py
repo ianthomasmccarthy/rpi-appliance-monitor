dc1aaa950b8a443879839eb43c5f382dc6c2b955
ianthomasmccarthy <ianthomasmccarthy@gmail.com>
2017-02-13 21:13:38 -0600

    adding major updates for testing

--- bin/vibration.py
+++ bin/vibration.py
@@ -1,13 +1,12 @@
 from telegram_send import send
-from threading import Thread
+# from threading import Thread
 
 import traceback
 import logging
 import datetime
 import RPi.GPIO
-import requests
-import json
-import sys
+# import json
+# import sys
 import time
 
 
@@ -20,11 +19,15 @@ class LaundryMassager(object):
         self.l_vib_time       = 0
         self.appliance_active = False
         self.active_message   = "Dryer has started."
-        self.stopped_message  = "Dryer has Stopped."
+        self.stopped_message  = "Dryer has Stopped. Duration {t} minutes."
         self.inactive_message = "Dryer has been inactive since {t}"
         self.log_file = "/home/pi/projects/vib.log"
         self.count = 0
         self.count_thresh = 40
+        self.sleep_interval = 60
+        self.stopped = 0
+        self.stopped_thresh = 3
+        self.inactive_thresh = 86400
 
     def get_logger(self):
         """Get Logger."""
@@ -39,11 +42,36 @@ class LaundryMassager(object):
         self.log.addHandler(handler)
 
     def vibrated(self, x):
-        #self.log.info("Vibrated")
+        # self.log.info("Vibrated")
         self.count += 1
+        if self.count < self.count_thresh:
+            return
         if self.count >= self.count_thresh:
-            self.log.info("Vibrated {c} times.".format(c=self.count))
-            self.count = 0
+            self.count = 0 # Reset a greater than count here.
+            self.l_vib_time = time.time()
+            if self.active:
+                return
+            if not self.active:
+                self.active = True
+                self.s_vib_time = time.time()
+                self.send_appliance_active()
+
+    def monitor(self):
+        while True:
+            try:
+                time.sleep(self.sleep_interval)
+                if self.active:
+                    if self.count < self.count_thresh:
+                        self.stopped += 1
+                        if self.stopped >= self.stopped_thresh:
+                            tot_time = time.time() - self.s_vib_time / 60
+                            self.send_appliance_stopped(duration=tot_time)
+                else:
+                    if self.l_vib_time > self.inactive_thresh:
+                        self.send_appliance_inactive()
+                        self.l_vib_time = time.time()
+            except Exception as e:
+                raise e
 
     def convert_timestamp(ts):
         """Return a string of datetime from a time.time() timestamp."""
@@ -52,8 +80,8 @@ class LaundryMassager(object):
     def send_appliance_active(self):
         self.send_alert(message=self.active_message)
 
-    def send_appliance_stopped(self):
-        self.send_alert(message=self.stopped_message)
+    def send_appliance_stopped(self, duration):
+        self.send_alert(message=self.stopped_message.format(t=duration))
 
     def send_appliance_inactive(self):
         self.send_alert(message=self.inactive_message.format(t=self.convert_timestamp(ts=self.l_vib_time)))
@@ -73,7 +101,6 @@ class LaundryMassager(object):
             RPi.GPIO.setmode(RPi.GPIO.BCM)
             RPi.GPIO.setup(sensor_pin, RPi.GPIO.IN, pull_up_down=RPi.GPIO.PUD_DOWN)
             RPi.GPIO.add_event_detect(sensor_pin, RPi.GPIO.RISING, callback=self.vibrated, bouncetime=1)
-            #RPi.GPIO.add_event_callback(sensor_pin, self.vibrated)
         except Exception as e:
             tb = traceback.format_exc()
             self.log.error("Problem setting up gpio pin monitoring. e:{e}; tb:{tb}".format(e=e, tb=tb))
@@ -87,5 +114,5 @@ class LaundryMassager(object):
             time.sleep(1)
 
 if __name__ == '__main__':
-   lm = LaundryMassager()
-   lm.main()
+    lm = LaundryMassager()
+    lm.main()
