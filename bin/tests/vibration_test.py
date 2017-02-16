32865e092076820e2c15bac78dfb772446f11077
ianthomasmccarthy <ianthomasmccarthy@gmail.com>
2017-02-15 21:22:19 -0600

    fixing unit tests

--- bin/tests/vibration_test.py
+++ bin/tests/vibration_test.py
@@ -1,6 +1,6 @@
 import unittest2 as unittest
 import mock
-from ..vibration import LaundryMassager
+from vibration import LaundryMassager
 
 
 class TestLaundryMassager(unittest.TestCase):
@@ -14,18 +14,19 @@ class TestLaundryMassager(unittest.TestCase):
     def test_simple(self):
         self.assertEquals(1, 1)
 
-    @mock.patch('vibration.time.time')
+    @mock.patch('vibration.time')
     def test_vibrated(self, mock_time):
-        mock_time.configure_mock(attrs={'method.return_value': 1})
+        mock_time.time.return_value = 1
         self._lm.vibrated(x=0)
         self.assertEquals(self._lm.count, 1)
         self.assertEquals(self._lm.l_vib_time, 1)
 
+#    @unittest.skip("Not the same on Windows")
     def test_convert_timestamp(self):
         td = self._lm.convert_timestamp(0)
-        self.assertEquals(td, "12/31/69 18:00:00")
+        self.assertEquals(td, "Thu Jan  1 00:00:00 1970")
         td = None
-        td = self._lm.convert_timestamp(-1)
+        td = self._lm.convert_timestamp(None)
         self.assertEquals(td, "Date Error")
 
     def test_send_appliance_active(self):
@@ -43,26 +44,26 @@ class TestLaundryMassager(unittest.TestCase):
         self._lm.send_appliance_inactive()
         assert self._lm.send_alert.called
 
-    @mock.patch('vibration.time.time')
+    @mock.patch('vibration.time')
     def test_start_active(self, mock_time):
         self.assertEquals(self._lm.appliance_active, False)
-        mock_time.configure_mock(attrs={'method.return_value': 1})
+        mock_time.time.return_value = 1
         self._lm.send_appliance_active = mock.MagicMock()
         self._lm.start_active()
         self.assertTrue(self._lm.appliance_active)
         self.assertEqual(self._lm.s_vib_time, 1)
         assert self._lm.send_appliance_active.called
 
-    @mock.patch('vibration.time.time')
-    def test_should_stop(self):
+    @mock.patch('vibration.time')
+    def test_should_stop(self, mock_time):
         self._lm.send_appliance_stopped = mock.MagicMock()
-        mock_time.configure_mock(attrs={'method.return_value': 100})
+        self._lm.appliance_active = True
+        mock_time.time.return_value = 10
         self._lm.l_vib_time = 1
         self._lm.stopped_thresh = 1
-        c = self._lm.should_stop()
+        self._lm.should_stop()
         self.assertFalse(self._lm.appliance_active)
-        self.assertEquals(c, 0)
-        # assert self._lm.send_appliance_stopped.called
+        assert self._lm.send_appliance_stopped.called
 
 if __name__ == '__main__':
     unittest.main()
